1b991f1917e6aa90dae69d7355d94325d1eb2703
Nataly Moskalenko <y.natali.moskalenko@gmail.com>
2020-10-06 16:17:34 +0300

    corrected struct folder

--- main.go
+++ main.go
@@ -2,15 +2,7 @@ package main
 
 import (
 
-	//"fmt"
-	//_ "github.com/go-sql-driver/mysql"
-
-	//"log"
-
-	//"net/http"
-
-	//"github.com/NM211077/testTask_techTechnorely/controllers"
-	"github.com/NM211077/testTask_techTechnorely/util"
+	"github.com/NM211077/testTask_techTechnorely/app"
 )
 
 func main() {
